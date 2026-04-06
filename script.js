f0e51a68e1a1a9963cf8650e807f9739aec168e4
kortay <kortaykokturk@gmail.com>
2026-04-06 17:03:21 +0300

    added a chat box

--- script.js
+++ script.js
@@ -162,3 +162,23 @@ for (let item of document.querySelectorAll(".buttons-settings div"))
         document.getElementById(item.dataset.page).style.display = "flex";
     });
 }
+
+let buton_item = document.getElementById("chat_button");
+buton_item.addEventListener("click", function()
+{
+    let input = document.getElementById("chat_input").value;
+    if (input != null && input != "")
+    {
+        const chat_msg_header = document.createElement("div");
+        chat_msg_header.classList.add("nameTag");
+        chat_msg_header.textContent = "Kullanıcı: "
+        const chat_msg = document.createElement("div");
+        chat_msg.classList.add("aiChat");
+        chat_msg.textContent = input;
+        document.querySelector(".chatbox").appendChild(chat_msg_header);
+        document.querySelector(".chatbox").appendChild(chat_msg);
+        document.getElementById("chat_input").value = "";
+        let objDiv = document.getElementById(".chatbox_wrapper");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    }
+})
